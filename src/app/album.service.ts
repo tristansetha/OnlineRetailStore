6f9bb132d60a817da8072e8c8c85b092ba94dcfb
Jared Hanson and Tristan Setha <jared+tristan@>
2019-04-09 14:03:59 -0700

    injecting services into multiple comonents (marketplace and albumdetail)

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -11,4 +11,12 @@ export class AlbumService {
     return ALBUMS;
   }
 
+  getAlbumById(albumId: number) {
+    for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+
 }
